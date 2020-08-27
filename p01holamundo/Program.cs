7efa60fc498c4c6272dbdce0d4da00d0edd22d33
kmponce <es.m.ponce@gmail.com>
2020-08-27 14:47:05 -0500

    hola

---
+++ p01holamundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace p01holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+            Console.WriteLine("Esta es una prueba de sw");
+        }
+    }
+}
