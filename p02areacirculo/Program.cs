3500a405e3b6c2dfb2b29022bf1aa2e6795b08eb
kmponce <es.m.ponce@gmail.com>
2020-08-27 14:56:11 -0500

    actualizacion desde vs

---
+++ p02areacirculo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+//Este programa calcula el area de un circulo a partir del radio
+//Karla Margarita Ponce Garcia
+namespace p02areacirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float radio = 0;
+            double area = 0;
+
+            Console.Clear();
+            Console.WriteLine("Dame el radio del circulo");
+            radio = float.Parse( Console.ReadLine() );
+            area = Math.PI * Math.pow(radio,2);
+
+            Console.WriteLine($"El area es {area}");
+        }
+    }
+}
---
+++ p03areatriangulo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace p03areatriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
