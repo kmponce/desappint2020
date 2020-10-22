a318920ad651f31e88d9cb9cf2926646d32affd2
kmponce <es.m.ponce@gmail.com>
2020-10-21 21:10:49 -0500

    Clases 6 y 8 de Oct 2020

---
+++ p30mvcmoviev2/Controllers/HelloWorldController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace p30mvcmovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        /*public string Index()
+        {
+            return "This is my default action...";
+        }*/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        /*public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }*/
+        // GET: /HelloWorld/Welcome/ 
+// Requires using System.Text.Encodings.Web;
+/*
+public string Welcome(string name, int numTimes = 1)
+{
+    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+}*/
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
---
+++ p30mvcmoviev2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace p30mvcmovie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
